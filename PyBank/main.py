0f8440f230ee2dcd16e2eda804f196f54ea51953
Laura Trentino <laura@trentino.com.au>
2021-09-24 19:34:20 +0800

    started looking for Tot Months and Total

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,39 @@
 import os
 import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
+#csvpath = os.path.join('C:/Users/Laura/Desktop/Homework/python-challenge/PyBank/Resources/budget_data.csv')
+
+months = []
+total = []
+change = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader: 
+        months.append(row[0])
+        total.append(int(row[1]))
+        
+# print(f'Total Months: {len(months)}')
+# print(f'Total: ${sum(total)}')
+
+# finding change...
+for row in total:
+    i = total.index(row)
+    if i > 0:
+        change.append(total[i]-total[i-1])
+print(change)
+
+
+
+
+
+
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-challenge
 # test
+# test2
