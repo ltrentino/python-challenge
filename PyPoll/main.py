de73e26b8d6bb7ea538329deb62bc51bfe0a05cf
Laura Trentino <laura@trentino.com.au>
2021-09-25 16:16:36 +0800

    kept going with just lists and for loops but realised different approach may be needed to automate things such as winner...

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,25 +30,32 @@ with open(csvpath, encoding='utf-8') as csvfile:
         elif row[2] == "O'Tooley":
             otooley.append(row)
 
-print(len(khan))
-print(len(correy))
-print(len(li))
-print(len(otooley))
-
 # to check unique candidates
-print(set(candidate)) # returns O'Tooley, 'Khan', 'Li', 'Correy' --> added these to lists created
+#print(set(candidate)) # returns O'Tooley, 'Khan', 'Li', 'Correy' --> added these to lists created
+
+# create variables for readability
+total_votes = (len(candidate))
+khan_votes = (len(khan))
+correy_votes = (len(correy))
+li_votes = (len(li))
+otooley_votes = (len(otooley))
+
+khan_ratio = round((khan_votes / total_votes * 100),3)
+correy_ratio = round((correy_votes / total_votes * 100),3)
+li_ratio = round((li_votes / total_votes * 100),3)
+otooley_ratio = round((otooley_votes / total_votes * 100),3)
+
+print(f'Khan: {khan_ratio}% ({khan_votes})')
+print(f'Correy: {correy_ratio}% ({correy_votes})')
+print(f'Li: {Li_ratio}% ({Li_votes})')
+print(f'O\'Tooley: {otooley_ratio}% ({otooley_votes})')
+print("----------------------------------")
+
 
 
 # # checking lists len is same
 # print(len(votes))   #3521001
 # print(len(county))
 # print(len(candidate))
-
-# # Printing results to terminal    
-# print("Election Results\n--------------------")
-# print(f'Total Votes: {len(votes)}')
-
-
-
 
 
