17531acbf1de2a3adaceb02e15f0be3dbe519739
Laura Trentino <laura@trentino.com.au>
2021-09-25 14:59:07 +0800

    started working on individual candidates stats

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,8 +3,13 @@ import os
 import csv
 csvpath = os.path.join('C:/Users/Laura/Desktop/Homework/python-challenge/PyPoll/Resources/election_data.csv')
 
-# finding Total Votes"
-votes = []
+# creating lists
+candidate = []
+khan = []
+correy = []
+li = []
+otooley = []
+
 
 with open(csvpath, encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -12,8 +17,38 @@ with open(csvpath, encoding='utf-8') as csvfile:
     #print(f"CSV Header: {csv_header}")
 
     for row in csvreader: 
-        votes.append(row[0])
+        # votes.append(row[0])
+        # county.append(row[1])
+        candidate.append(row[2])
+        
+        if row[2] == "Khan":
+            khan.append(row)
+        elif row[2] == "Correy":
+            correy.append(row)
+        elif row[2] == "Li":
+            li.append(row)
+        elif row[2] == "O'Tooley":
+            otooley.append(row)
 
-# Printing results to terminal    
-print("Election Results\n--------------------")
-print(f'Total Votes: {len(votes)}')
+print(len(khan))
+print(len(correy))
+print(len(li))
+print(len(otooley))
+
+# to check unique candidates
+print(set(candidate)) # returns O'Tooley, 'Khan', 'Li', 'Correy' --> added these to lists created
+
+
+# # checking lists len is same
+# print(len(votes))   #3521001
+# print(len(county))
+# print(len(candidate))
+
+# # Printing results to terminal    
+# print("Election Results\n--------------------")
+# print(f'Total Votes: {len(votes)}')
+
+
+
+
+
